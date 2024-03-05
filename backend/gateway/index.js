4b960265b452ad5471338aaf8216dcded3da7f25
Martin Nilsson <martin.no.nilsson@gmail.com>
2024-03-05 20:01:36 +0100

    Fixed dynamic gateway and proxy URLs

--- backend/gateway/index.js
+++ backend/gateway/index.js
@@ -1,26 +1,30 @@
 const express = require("express");
-
 const cors = require("cors");
-
-const proxy = require("express-http-proxy");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+require("dotenv").config();
 
 const app = express();
 
+const { DEV_GATEWAY_URL, PROD_GATEWAY_URL, CLIENT_PROD_ORIGIN, CLIENT_DEV_ORIGIN } = process.env;
+const isDevEnv = process.env.NODE_ENV === "dev";
+
+const proxyURL = isDevEnv ? DEV_GATEWAY_URL : PROD_GATEWAY_URL;
+
+const userProxy = createProxyMiddleware({ target: `${proxyURL}:5001`, changeOrigin: true });
+const meetingProxy = createProxyMiddleware({ target: `${proxyURL}:5002`, changeOrigin: true });
+
 app.use(
 	cors({
-		origin: ["http://108.141.250.5:3000", "http://localhost:3000"],
-
+		origin: [CLIENT_PROD_ORIGIN, CLIENT_DEV_ORIGIN],
 		methods: ["GET", "POST", "PUT", "DELETE"],
-
 		credentials: true,
 	})
 );
 
 app.use(express.json());
 
-app.use("/user", proxy("http://108.141.250.5:5001"));
-
-app.use("/meeting", proxy("http://108.141.250.5:5002"));
+app.use("/user", userProxy);
+app.use("/meeting", meetingProxy);
 
 app.listen(5000, () => {
 	console.log("Gateway is Listening to Port 5000");
